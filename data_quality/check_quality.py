b15edc6af378586422265870c674cf2da9fa17fe
Ruta Stasytyte <ruta.stasytyte@gjensidige.no>
2022-06-01 10:31:41 +0300

    third commit

--- data_quality/check_quality.py
+++ data_quality/check_quality.py
@@ -2,7 +2,13 @@ import pandas as pd
 import logging
 
 
-def log_duplicates_in_column(df, column_list):
+def log_duplicates_in_column(df: pd.DataFrame, column_list: list):
+    """A function to check for duplicates in a list of columns.
+
+    Args:
+        df (pd.DataFrame): dataframe on which to perform quality checks.
+        column_list (list): list of columns that might contain duplicates.
+    """
 
     for column in column_list:
 
@@ -11,11 +17,14 @@ def log_duplicates_in_column(df, column_list):
         logging.info(
             f"There are {duplicated_values_count} duplicated values in {column} column."
         )
-
-    return
     
 
-def check_data_quality(df):
+def check_data_quality(df: pd.DataFrame) -> None:
+    """Wrapper function that logs checks data quality.
+
+    Args:
+        df (_type_): Dataframe on which to perform quality checks.
+    """
 
     log_duplicates_in_column(df, ["policy_id"])
     logging.info(f"There are {len(df[df.duplicated()])} dupicated rows.")
--- workflow/transform_data.py
+++ workflow/transform_data.py
@@ -5,7 +5,16 @@ import pandas as pd
 import dateutil.parser as parser
 
 
-def fillna_with_mode(df, column_list):
+def fillna_with_mode(df: pd.DataFrame, column_list: list) -> pd.DataFrame:
+    """Funtion to fill in provided columns with their most often occuring values.
+
+    Args:
+        df (pd.DataFrame): dataframe on which to perform transformations.
+        column_list (list): a list of columns on which need their values filled in.
+
+    Returns:
+        pd.DataFrame: a dataframe where the columns which were provided now don't have missing values.
+    """
 
     [df.fillna(df[column].mode(), inplace=True) for column in column_list]
 
@@ -13,13 +22,33 @@ def fillna_with_mode(df, column_list):
 
     return df
 
-def fillna_with_int(df, column_list, int):
+def fillna_with_int(df: pd.DataFrame, column_list: list, int: int) -> pd.DataFrame:
+    """Funtion to fill in provided columns with an integer.
+
+    Args:
+        df (pd.DataFrame): dataframe on which to perform transformations.
+        column_list (list):  a list of columns on which need their values filled in.
+        int (int): integer to use as filler for missing values.
+
+    Returns:
+        pd.DataFrame: a dataframe where the columns which were provided now don't have missing values.
+    """
 
     df[column_list] = df[column_list].fillna(int)
 
     return df
 
-def replace_values(df, column_list, value_dictionary):
+def replace_values(df: pd.DataFrame, column_list, value_dictionary) -> pd.DataFrame:
+    """Funtion to replace values within provided columns.
+
+    Args:
+        df (pd.DataFrame): dataframe on which to perform transformations.
+        column_list (_type_): a list of columns on which need their values replaced.
+        value_dictionary (_type_): dictionary used to specify different replacement values for different existing values.
+
+    Returns:
+        pd.DataFrame: a dataframe where the columns which were provided now have their values replaced
+    """
 
     df[column_list] = df[column_list].replace(value_dictionary, regex=True)
 
@@ -27,7 +56,16 @@ def replace_values(df, column_list, value_dictionary):
 
     return df
 
-def parse_dates(df, column_list):
+def parse_dates(df: pd.DataFrame, column_list: list) -> pd.DataFrame:
+    """Funtion that parses dates for a provided list of columns.
+
+    Args:
+        df (pd.DataFrame): dataframe on which to perform transformations.
+        column_list (list): a list of columns which need to be parsed to dates
+
+    Returns:
+        pd.DataFrame: a dataframe where date columns and of datetime.datetime.
+    """
     
     for column in column_list:
         df[column] = df[column].apply(parser.parse)
@@ -36,7 +74,7 @@ def parse_dates(df, column_list):
 
     return df
 
-def group_column(df, column, bins, labels:list=None):
+def group_column(df: pd.DataFrame, column: str, bins: list, labels:list=None) -> pd.DataFrame:
     
     df[f'{column}_group'] = pd.cut(x=df[f'{column}'], bins=bins, right=False, labels=labels)
 
@@ -44,7 +82,16 @@ def group_column(df, column, bins, labels:list=None):
 
     return df
 
-def transform(df):
+def transform(df: pd.DataFrame) -> pd.DataFrame:
+    """Wrapper function that calls multiple transformation functions on the data.
+
+    Args:
+        df (pd.DataFrame): Dataframe on which to perform transformations.
+
+    Returns:
+        pd.DataFrame: a dataframe on which transformations where performed.
+    """
+
 
     df = fillna_with_mode(df, ["car_brand", "postal_code"])
     df = fillna_with_int(df,["car_eng_pow", "customer_age"], -1)
